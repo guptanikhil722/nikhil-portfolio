94f11638328b68125e90bd02bdeb3303373999ab
guptanikhil722 <guptanikhil722>
2023-08-17 01:43:06 +0530

    new

--- src/components/Skillbar/Skillbar.js
+++ src/components/Skillbar/Skillbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from './Skillbar.module.css';
 
 const SkillBar = ({ skillName }) => {
@@ -21,7 +21,7 @@ const SkillBar = ({ skillName }) => {
       
         </div>
         </>
-      )  
+      ) ;
         })
       }
       
